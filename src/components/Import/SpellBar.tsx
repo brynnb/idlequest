3c6aea9d89f2c75cedf7ffbc2fce9941c008012a
brynnb <brynnb@users.noreply.github.com>
2024-10-12 13:12:33 -0500

    Mostly fix spell bar styling

--- src/components/Import/SpellBar.tsx
+++ src/components/Import/SpellBar.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import styled from "styled-components";
 import { getSpellGems, SpellGem } from "../../utils/uiUtils";
 
-const SCALE_FACTOR = 1.7; //using scale factor to properly size the gems and make adjustment easier as UI tweaks are made
+const SCALE_FACTOR = 1.8; //using scale factor to properly size the gems and make adjustment easier as UI tweaks are made
 
 const SpellBarContainer = styled.div.attrs({
   className: "spell-bar-container",
 })`
-  width: 70px;
+  width:70px;
   right: 0px;
   position: absolute;
   padding-top: 2px;
@@ -16,9 +16,9 @@ const SpellBarContainer = styled.div.attrs({
 const SpellGemDiv = styled.div.attrs({
   className: "spell-gem",
 })<{ scaleFactor: number }>`
-  margin-top: ${21 * SCALE_FACTOR}px;
+  margin-top: ${18.5 * SCALE_FACTOR}px;
   width: ${38 * SCALE_FACTOR}px;
-  height: ${29 * SCALE_FACTOR}px;
+  height: ${28.9 * SCALE_FACTOR}px;
 `;
 
 const SpellBar: React.FC = () => {
@@ -35,7 +35,7 @@ const SpellBar: React.FC = () => {
             background: `url('/images/${gem.spritesheet}') ${
               gem.x * SCALE_FACTOR
             }px ${gem.y * SCALE_FACTOR}px no-repeat`,
-            backgroundSize: `${263 * SCALE_FACTOR}px`,
+            backgroundSize: `${263.3 * SCALE_FACTOR}px`,
           }}
         />
       ))}
